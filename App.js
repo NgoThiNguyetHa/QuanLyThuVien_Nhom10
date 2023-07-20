fa10fd330b88fab2a79eaa7d427a6e9a3d5cc467
NgoThiNguyetHa <hantnph28876@fpt.edu.vn>
2023-07-20 14:18:42 +0700

    ManHinhChao

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ManHinhChao from './Screen/ManHinhChao';
+import PhieuMuon from './Screen/PhieuMuon';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ManHinhChao" component={ManHinhChao} />
+        <Stack.Screen name="PhieuMuon" component={PhieuMuon} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
