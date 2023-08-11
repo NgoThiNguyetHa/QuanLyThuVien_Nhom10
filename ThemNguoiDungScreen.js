3456bf9e6cc964b91cd618095c637fb5585a4b99
HoangLong <hoanglong180903@gmail.com>
2023-08-12 00:20:08 +0700

    fix loi thanh vien

--- ThemNguoiDungScreen.js
+++ ThemNguoiDungScreen.js
@@ -1,13 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { FlatList, Image, StyleSheet, Text, View , Alert, Modal, } from "react-native";
-import { Button, TextInput, TouchableOpacity } from "react-native-web";
+import { FlatList, Image, StyleSheet, Text, View , Alert, Modal, Button, TextInput, TouchableOpacity } from "react-native";
 // import axios from 'axios';
 export default function ThemNguoiDung() {
-  const [username, setUsername] = useState("Tên đăng nhập");
-  const [fullname, setFullname] = useState("Họ tên");
-  const [password, setPassword] = useState("password");
-  const [rePassword, setRePassword] = useState("Nhập lại password");
+  const [username, setUsername] = useState("");
+  const [hoTen, setHoTen] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
   //set list nguoi dung
   const [idLoading, setIsLoading] = useState(true);
   const [dsnd, setdsnd] = useState([]);
@@ -19,18 +18,7 @@ export default function ThemNguoiDung() {
     // getListNguoiDung();
     getListND();
   })
-  // const getListNguoiDung = async () => {
-  //   let url_api = "https://64b89a9421b9aa6eb079f300.mockapi.io/nguoidung";
-  //   try {
-  //     const reponse = await fetch(url_api); //load data
-  //     const json = await reponse.json(); // chuyen data thanh json
-  //     setdsnd(json); 
-  //   } catch (err) {
-  //     console.error(err);
-  //   } finally {
-  //     setIsLoading(false); 
-  //   }
-  // };
+  
   const getListND = () => {
     fetch("https://64b89a9421b9aa6eb079f300.mockapi.io/nguoidung", {
       method:"GET"
@@ -47,56 +35,63 @@ export default function ThemNguoiDung() {
 
 
   //post data to api
-  const handleSubmit = async (err) => {
-    err.preventDefault();
+  // const handleSubmit = async (err) => {
+  //   err.preventDefault();
+  //   const allInputValue = {
+  //     username: username,
+  //     hoTen: hoTen,
+  //     password: password,
+  //     rePassword: rePassword,
+  //   };
+  //   let res = await fetch("https://64b89a9421b9aa6eb079f300.mockapi.io/nguoidung", {
+  //     method: "POST",
+  //     headers: { "content-type": "application/json" },
+  //     body: JSON.stringify(allInputValue),
+  //   });
+    
+  //   console.log(allInputValue);
+  // };
+  const handleSubmit = () => {
     const allInputValue = {
       username: username,
-      fullname: fullname,
+      hoTen: hoTen,
       password: password,
       rePassword: rePassword,
     };
-    let res = await fetch("https://64b89a9421b9aa6eb079f300.mockapi.io/nguoidung", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(allInputValue),
-    });
-    
+    fetch('http://192.168.1.135:3000/insertNguoiDung', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(allInputValue),
+    })
+    Alert.alert("Them thanh cong");
+    setUsername("");
+    setHoTen("")
+    setPassword("")
+    setRePassword("")
     console.log(allInputValue);
   };
-  //delete
-  const handleRemove = (user_id) => {
-     fetch(`https://64b89a9421b9aa6eb079f300.mockapi.io/nguoidung/${user_id}`, {
-       method: "DELETE",
-       headers: {
-         'Accept': "application/json",
-         "Content-Type": "application/json",
-       },
-     })
-       .then((res) => {
-         return res.json()})
-       .then((res) => {
-         console.log(res);
-         getListNguoiDung();
-       })
-       .catch((err) => {
-         console.log(err);
-       });
-  }
+
 
   return (
     <View style={styles.container}>
       <Image
         style={styles.stretch}
-        source={require("../assets/image 44.png")}
+        source={{
+          uri: 'https://cdn.dribbble.com/users/1804673/screenshots/9443949/media/f114a64890767d899a3374fc7a1ef612.png?resize=400x0',
+        }}
       />
       <View style={styles.sectionStyle}>
         {/* tên đăng nhập */}
         <Image
-          source={require("../assets/image 46.png")}
+         source={{
+          uri: 'https://cdn-icons-png.flaticon.com/512/668/668709.png',
+        }}
           style={styles.imageStyle}
         />
         <TextInput
-          placeholder={username}
+          placeholder="Nhập username"
           onChangeText={(newText) => setUsername(newText)}
           style={{
             flex: 1,
@@ -113,12 +108,14 @@ export default function ThemNguoiDung() {
       <View style={styles.sectionStyle}>
         {/* họ tên user */}
         <Image
-          source={require("../assets/image 42.png")}
+          source={{
+            uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7K0UoVnOQAG7tXzKBYaKX6aLCxzSIpIEXlrjrJK2VU-mOT8HBwGIRypGqxzXQqrrWfy8&usqp=CAU"
+          }}
           style={styles.imageStyle}
         />
         <TextInput
-          placeholder={fullname}
-          onChangeText={(newText) => setFullname(newText)}
+          placeholder="Nhập họ tên"
+          onChangeText={(newText) => setHoTen(newText)}
           style={{ flex: 1, borderColor: "#fff", padding: 10 }}
           underlineColorAndroid="transparent"
           mode="outlined"
@@ -127,11 +124,13 @@ export default function ThemNguoiDung() {
       <View style={styles.sectionStyle}>
         {/* password */}
         <Image
-          source={require("../assets/image 45.png")}
+          source={{
+            uri:"https://cdn-icons-png.flaticon.com/512/6146/6146586.png"
+          }}
           style={styles.imageStyle}
         />
         <TextInput
-          placeholder={password}
+          placeholder="Nhập password"
           onChangeText={(newText) => setPassword(newText)}
           style={{ flex: 1, borderColor: "#fff", padding: 10 }}
           underlineColorAndroid="transparent"
@@ -141,11 +140,13 @@ export default function ThemNguoiDung() {
       <View style={styles.sectionStyle}>
         {/* re-password */}
         <Image
-          source={require("../assets/image 47.png")}
+          source={{
+            uri:"https://cdn-icons-png.flaticon.com/512/6357/6357048.png"
+          }}
           style={styles.imageStyle}
         />
         <TextInput
-          placeholder={rePassword}
+          placeholder="Nhập lại mật khẩu"
           onChangeText={(newText) => setRePassword(newText)}
           style={{ flex: 1, borderColor: "#fff", padding: 10 }}
           underlineColorAndroid="transparent"
@@ -188,69 +189,6 @@ export default function ThemNguoiDung() {
       </View>
 
       {/* flat list  */}
-      <FlatList
-        data={dsnd}
-        keyExtractor={(item , index) => index.toString()}
-        renderItem={({ item }) => {
-          return (
-            <View style={{ borderBottomWidth: 1, borderBottomColor: "#fff" }}>
-              <Image
-                style={{ width: 50, height: 50 }}
-                source={(require = "/assets/image 46.png")}
-              />
-              <Text>username: {item.username}</Text>
-              <Text>fullname: {item.fullname}</Text>
-              <Text
-                onPress={() => {
-                  setModalVisible(true);
-                }}
-                style={{ fontWeight: "bold", color: "red" }}
-              >
-                Delete
-              </Text>
-              <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                  setModalVisible(!modalVisible);
-                }}
-              >
-                <View style={styles.centeredView}>
-                  <View style={styles.modalView}>
-                    <Text style={styles.modalText}>
-                      Bạn có chắc chắn muốn xóa không!
-                    </Text>
-                    <View
-                      style={{
-                        flexDirection: "row",
-                        width: "100%",
-                        justifyContent: "space-around",
-                      }}
-                    >
-                      <TouchableOpacity
-                        style={[styles.button, styles.buttonClose]}
-                        onPress={() => {
-                          setModalVisible(!modalVisible), handleRemove(item.id);
-                        }}
-                      >
-                        <Text style={styles.textStyle}>Yes</Text>
-                      </TouchableOpacity>
-                      {/* cancel  */}
-                      <TouchableOpacity
-                        style={[styles.button, styles.buttonClose]}
-                        onPress={() => setModalVisible(!modalVisible)}
-                      >
-                        <Text style={styles.textStyle}>No</Text>
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </View>
-              </Modal>
-            </View>
-          );
-        }}
-      />
       <StatusBar style="auto" />
     </View>
   );
@@ -261,26 +199,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    padding:15
   },
   stretch: {
-    width: 150,
-    height: 100,
-    resizeMode: "stretch",
+    width: 200,
+    height: 200,
   },
   inputContainer: {
     justifyContent: "center",
   },
   sectionStyle: {
     flexDirection: "row",
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent:"center",
     backgroundColor: "#fff",
     borderWidth: 0.5,
-    borderColor: "#000",
-    height: 40,
+    borderColor: "#000000",
+    height: 50,
     borderRadius: 5,
     margin: 10,
+    padding:5
   },
   imageStyle: {
     padding: 10,
